3e236a2537e8256ebb2a88459c66029bc27ecc1f
gchamore <gchamore@student.42.fr>
2025-02-04 14:47:37 +0100

    okok

--- includes/ServerMessages.hpp
+++ includes/ServerMessages.hpp
@@ -7,17 +7,9 @@ namespace ServerMessages
 {
 	// Numeric Replies (001-099)
 	const std::string RPL_WELCOME = "001";
-	const std::string RPL_YOURHOST = "002";
-	const std::string RPL_CREATED = "003";
-	const std::string RPL_MYINFO = "004";
 
 	// Command responses (200-399)
-	const std::string RPL_WHOISUSER = "311";
 	const std::string RPL_ENDOFWHO = "315";
-	const std::string RPL_ENDOFWHOIS = "318";
-	const std::string RPL_WHOISCHANNELS = "319";
-	const std::string RPL_LIST = "322";
-	const std::string RPL_LISTEND = "323";
 	const std::string RPL_CHANNELMODEIS = "324";
 	const std::string RPL_NOTOPIC = "331";
 	const std::string RPL_TOPIC = "332";
@@ -26,24 +18,13 @@ namespace ServerMessages
 	const std::string RPL_ENDOFNAMES = "366";
 
 	// WHO/WHOIS related responses (310-369)
-	const std::string RPL_WHOREPLY = "352";	 // "<channel> <user> <host> <server> <nick> <H|G>[*][@|+] :<hopcount> <real name>"
-	const std::string RPL_WHOSPCRPL = "354"; // Format dépend des tokens demandés
-
-	// MOTD related responses (370-399)
-	const std::string RPL_MOTD = "372";		 // ":- <text>"
-	const std::string RPL_MOTDSTART = "375"; // ":- <server> Message of the day - "
-	const std::string RPL_ENDOFMOTD = "376"; // ":End of MOTD command"
+	const std::string RPL_WHOREPLY = "352";
 
     // Error messages (400-599)
     const std::string ERR_NOSUCHNICK = "401";
-    const std::string ERR_NOSUCHSERVER = "402";
     const std::string ERR_NOSUCHCHANNEL = "403";
-    const std::string ERR_CANNOTSENDTOCHAN = "404";
     const std::string ERR_TOOMANYCHANNELS = "405";
-    const std::string ERR_WASNOSUCHNICK = "406";
-    const std::string ERR_NOORIGIN = "409";
     const std::string ERR_NORECIPIENT = "411";
-    const std::string ERR_NOTEXTTOSEND = "412";
     const std::string ERR_UNKNOWNCOMMAND = "421";
     const std::string ERR_NONICKNAMEGIVEN = "431";
     const std::string ERR_ERRONEUSNICKNAME = "432";
@@ -58,32 +39,10 @@ namespace ServerMessages
     const std::string ERR_CHANNELISFULL = "471";
     const std::string ERR_UNKNOWNMODE = "472";
     const std::string ERR_INVITEONLYCHAN = "473";
-    const std::string ERR_BANNEDFROMCHAN = "474";
     const std::string ERR_BADCHANNELKEY = "475";
     const std::string ERR_CHANOPRIVSNEEDED = "482";
 
     const std::string ERR_UMODEUNKNOWNFLAG = "501";
-
-	// Message templates
-	namespace Templates
-	{
-		const std::string WELCOME = ":server " + RPL_WELCOME + " %s :Welcome to the IRC Network, %s!";
-		const std::string NICK_IN_USE = ":server " + ERR_NICKNAMEINUSE + " * %s :Nickname is already in use";
-		const std::string INVALID_NICK = ":server " + ERR_ERRONEUSNICKNAME + " * :Invalid nickname";
-		const std::string NO_SUCH_CHANNEL = ":server " + ERR_NOSUCHCHANNEL + " %s :No such channel";
-		const std::string NOT_ON_CHANNEL = ":server " + ERR_NOTONCHANNEL + " %s :You're not on that channel";
-		const std::string NEED_MORE_PARAMS = ":server " + ERR_NEEDMOREPARAMS + " %s :Not enough parameters";
-		const std::string CHANNEL_IS_FULL = ":server " + ERR_CHANNELISFULL + " %s :Cannot join channel (+l)";
-		const std::string INVITE_ONLY = ":server " + ERR_INVITEONLYCHAN + " %s :Cannot join channel (+i)";
-		const std::string BAD_CHANNEL_KEY = ":server " + ERR_BADCHANNELKEY + " %s :Cannot join channel (+k)";
-		const std::string NOT_CHANNEL_OPERATOR = ":server " + ERR_CHANOPRIVSNEEDED + " %s :You're not channel operator";
-
-		// WHO/WHOIS templates
-		const std::string WHO_REPLY = ":server " + RPL_WHOREPLY + " %s %s %s %s %s H%s :0 %s";
-		const std::string WHO_END = ":server " + RPL_ENDOFWHO + " %s :End of WHO list";
-		const std::string NAMES_REPLY = ":server " + RPL_NAMREPLY + " = %s :%s";
-		const std::string NAMES_END = ":server " + RPL_ENDOFNAMES + " %s :End of NAMES list";
-	}
 }
 
 #endif
--- srcs/ServerCommands.cpp
+++ srcs/ServerCommands.cpp
@@ -6,7 +6,7 @@
 /*   By: gchamore <gchamore@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 15:32:35 by anferre           #+#    #+#             */
-/*   Updated: 2025/02/04 12:01:35 by gchamore         ###   ########.fr       */
+/*   Updated: 2025/02/04 14:31:02 by gchamore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,6 +55,8 @@ void Server::handleCommand(const CommandParser::ParsedCommand &command, Client &
 			if (DEBUG_MODE)
 				std::cout << "is Authenticated: " << client.authenticated() << std::endl;
 		}
+		else
+			client.sendResponse(":server " + ServerMessages::ERR_NOTREGISTERED + " * :You must authenticate first with PASS command");
 		return; // Silently ignore other commands until authenticated
 	}
 
@@ -293,7 +295,7 @@ void Server::handleUserCommand(const CommandParser::ParsedCommand &command, Clie
 	client.sendResponse(":server NOTICE Auth :Username successfully set to " + command.params[0]);
 
 	// Puis envoyer le message de bienvenue
-	client.sendResponse(":server 001 " + client.getNickname() +
+	client.sendResponse(":server " + ServerMessages::RPL_WELCOME + client.getNickname() +
 						" :Welcome to the IRC Network, " + client.getNickname() + "!");
 }
 
@@ -606,7 +608,6 @@ void Server::handleWhoCommand(const CommandParser::ParsedCommand &command, Clien
 									" " + target->getNickname() +	 // nickname
 									" H" +							 // Here (H) or Gone (G)
 									" :0 " + target->getUsername()); // hopcount et realname
-				std::cout << client.getNickname() << " * " << target->getUsername() << " localhost irc.server " << target->getNickname() << " H :0 " << target->getUsername() << std::endl;
 			}
 		}
 		client.sendResponse(":server " + ServerMessages::RPL_ENDOFWHO + " " + client.getNickname() + " :End of WHO list");
@@ -633,7 +634,6 @@ void Server::handleWhoCommand(const CommandParser::ParsedCommand &command, Clien
 									" " + (*it)->getNickname() +	// nickname
 									" H" + prefix +					// Here + operator status
 									" :0 " + (*it)->getUsername()); // hopcount + realname
-				std::cout << client.getNickname() << " " << target << " " << (*it)->getUsername() << " localhost irc.server " << (*it)->getNickname() << " H :0 " << (*it)->getUsername() << std::endl;
 			}
 
 			// Message de fin
